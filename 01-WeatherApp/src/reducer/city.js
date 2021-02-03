48355305eb22c82bea2e4cc78120538ba669a33d
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2021-02-03 00:45:06 -0500

    cambios del reducer

---
+++ 01-WeatherApp/src/reducer/city.js
@@ -0,0 +1,15 @@
+import {SET_CITY} from '../actions'
+
+
+//reducer
+export const reducer = (state,action)=>{
+    switch (action.type){
+        case SET_CITY:
+            return {...state,city:action.value}
+
+
+        default:
+            return state;
+    }
+
+}
--- 01-WeatherApp/src/store/index.js
+++ 01-WeatherApp/src/store/index.js
@@ -1,5 +1,12 @@
 //creamos el store y pasamos el reduxser
 import {createStore} from "redux";
+import {reducer} from '../reducer/city'
 
-export const store = createStore(()=>{},
+//estado inicial
+const initialState = {
+    city:'Quito,ec'
+}
+
+
+export const store = createStore(reducer,initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
