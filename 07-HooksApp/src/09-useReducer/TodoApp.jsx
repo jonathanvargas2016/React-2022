c215fbded5af6be00148d7e9126bda568319daab
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2022-09-06 23:12:11 -0500

    Implementation onDelete Todo

--- 07-HooksApp/src/09-useReducer/TodoApp.jsx
+++ 07-HooksApp/src/09-useReducer/TodoApp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useReducer } from 'react'
 import { todoReducer } from './todo-reducer'
 import { TodoAdd } from './TodoAdd'
@@ -7,22 +8,36 @@ import { TodoList } from './TodoList'
 export const TodoApp = () => {
 
     const initialState = [
-        {
-            id: new Date().getTime(),
-            description: 'Recolectar la piedra del alma',
-            done: false
-        },
-        {
-            id: new Date().getTime() * 3,
-            description: 'Recolectar la piedra del poder',
-            done: false
-        }
+        // {
+        //     id: new Date().getTime(),
+        //     description: 'Recolectar la piedra del alma',
+        //     done: false
+        // }
     ]
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
+    const init = () => { // funcion para los datos iniciales
+        return JSON.parse(localStorage.getItem('todos')) || []
+    }
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    const handleDeleteTodo = id => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
 
     const handleNewTodo = (todo) => {
-        console.log({todo})
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action)
     }
 
     return (
@@ -32,14 +47,17 @@ export const TodoApp = () => {
 
             <div className='row'>
                 <div className='col col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12'>
-                    <TodoList todos={todos} />
+                    <TodoList todos={todos} onDeleteTodo={id => handleDeleteTodo(id)} />
+                    {/* id => es el valor q el evento onDeleteTodo 
+                    emite y ese valor le paso a la funcion handleDeleteTodo */}
+
                 </div>
                 <div className='col col-xl-5 col-lg-5 col-md-5 col-sm-5 col-12'>
                     <h4>Agregar TODO</h4>
                     <hr />
-                
+
                     {/* <TodoAdd onNewTodo={e => handleNewTodo(e) } /> */}
-                    <TodoAdd onNewTodo={handleNewTodo } />
+                    <TodoAdd onNewTodo={handleNewTodo} />
 
                 </div>
             </div>
