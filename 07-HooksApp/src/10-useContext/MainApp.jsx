b380abe877c282b1bc9f9c33ec9bb87f9519da82
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2022-09-08 23:03:10 -0500

    Implementation of  Link tag

--- 07-HooksApp/src/10-useContext/MainApp.jsx
+++ 07-HooksApp/src/10-useContext/MainApp.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Link, Navigate, Route, Routes } from 'react-router-dom'
 import { HomePage } from './HomePage'
 import { AboutPage } from './AboutPage'
 import { LoginPage } from './LoginPage'
+import { Navbar } from './Navbar'
 
 export const MainApp = () => {
     return (
@@ -10,13 +11,16 @@ export const MainApp = () => {
             <h1>MainApp</h1>
             <hr />
 
+            <Navbar />
+
+
             <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='login' element={<LoginPage />} />
                 <Route path='about' element={<AboutPage />} />
 
                 {/* <Route path='/*' element={<LoginPage />} /> */}
-            
+
                 {/* navegar a una ruta mas especifica. Navigate. componente q a penas se renderiza hace la navegacion al path  especificado*/}
                 <Route path='/*' element={<Navigate to="/about" />} />
             </Routes>
